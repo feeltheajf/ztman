4162ab579fcb98e5231d0349981f330ed377b126
Ilya Radostev <feeltheajf@gmail.com>
2021-08-12 00:46:08 +0500

    Use default format by zerolog

--- logging/logging.go
+++ logging/logging.go
@@ -3,7 +3,6 @@ package logging
 import (
 	"os"
 
-	"github.com/fatih/color"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
@@ -15,38 +14,8 @@ func Setup(level zerolog.Level) {
 	zerolog.SetGlobalLevel(level)
 	log.Logger = log.Output(
 		zerolog.ConsoleWriter{
-			Out:         os.Stderr,
-			NoColor:     config.Sht,
-			FormatLevel: formatLevel,
+			Out:     os.Stderr,
+			NoColor: config.Sht,
 		},
 	)
-}
-
-func formatLevel(level interface{}) string {
-	if levelString, ok := level.(string); ok {
-		switch levelString {
-		case "trace":
-			return colorize("[T]", color.FgMagenta)
-		case "debug":
-			return colorize("[D]", color.FgCyan)
-		case "info":
-			return colorize("[I]", color.FgGreen)
-		case "warn":
-			return colorize("[W]", color.FgYellow)
-		case "error":
-			return colorize("[E]", color.FgRed)
-		case "fatal":
-			return colorize("[F]", color.FgRed, color.Bold)
-		case "panic":
-			return colorize("[P]", color.FgRed, color.Bold)
-		}
-	}
-	return colorize("[?]", color.Bold)
-}
-
-func colorize(s string, c ...color.Attribute) string {
-	if config.Sht {
-		return s
-	}
-	return color.New(c...).Sprint(s)
 }
