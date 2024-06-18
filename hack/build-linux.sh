6b748b69aec4ac54a0b86d0e97de56a92961e799
Ilya Radostev <feeltheajf@gmail.com>
2024-06-18 21:06:05 +0200

    Fix spaces

--- hack/build-linux.sh
+++ hack/build-linux.sh
@@ -10,13 +10,13 @@ rm -rf $DIST/*
 
 for ARCH in amd64; do
     OUT="$DIST/ztman_linux_$ARCH"
-	docker run -i --rm \
-		-v "$(pwd):/go/src/github.com/feeltheajf/ztman:rw" \
-		-w /go/src/github.com/feeltheajf/ztman \
-		-e CGO_ENABLED=1 \
-		ztman-xgo \
-		--build-cmd "go build -ldflags \"-s -w -X main.version=$VERSION\" -o $OUT" \
-		-p "linux/amd64"
-	cp "$OUT" "$BIN"
+    docker run -i --rm \
+        -v "$(pwd):/go/src/github.com/feeltheajf/ztman:rw" \
+        -w /go/src/github.com/feeltheajf/ztman \
+        -e CGO_ENABLED=1 \
+        ztman-xgo \
+        --build-cmd "go build -ldflags \"-s -w -X main.version=$VERSION\" -o $OUT" \
+        -p "linux/amd64"
+    cp "$OUT" "$BIN"
     zip "$OUT.zip" "$BIN" README.md VERSION
 done
